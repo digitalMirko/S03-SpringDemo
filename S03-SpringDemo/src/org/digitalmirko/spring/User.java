ad19468517e9a1a01f35a3e49aa94a36b60c4f96
Mirko Cukich <mirkocukich@gmail.com>
2016-11-02 15:09:03 -0500

    Implementing Inversion of Control

--- S03-SpringDemo/src/org/digitalmirko/spring/User.java
+++ S03-SpringDemo/src/org/digitalmirko/spring/User.java
@@ -1,5 +1,8 @@
 package org.digitalmirko.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class User {
 
 	public static void main(String[] args) {
@@ -10,8 +13,14 @@ public class User {
 		// Bike Insurance instance
 //		Insurance status = new bikeInsurance();
 		
-		Insurance status = new carInsurance();
+//		Insurance status = new carInsurance();
+//		System.out.println(status.showStatus());
+		
+		//Application Context interface
+		FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("Beans.xml");
+		Insurance status = context.getBean("myInsurance", Insurance.class);
 		System.out.println(status.showStatus());
+		context.close();
 		
 	}
 
